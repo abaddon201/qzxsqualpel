c78ea2d015144d6a842b4439e00bf9fc285d8a87
abaddon201 <aglebov2@gmail.com>
2019-11-28 09:16:11 +0300

    finished register loading scan

--- core/disassembler_core.cpp
+++ core/disassembler_core.cpp
@@ -397,22 +397,37 @@ void DisassemblerCore::updateRegisterSource(ChunkPtr chunk, int idx, ArgPtr arg,
   }
   auto arg_ref = std::static_pointer_cast<ArgRegisterReference>(arg);
   auto reg = arg_ref->reg_id;
+  updateRegisterSource(chunk, idx, reg, size);
+}
+
+void DisassemblerCore::updateRegisterSource(ChunkPtr chunk, int idx, Register16 reg, ArgSize size) {
   for (int i = idx; i >= 0; i--) {
     auto& cmd = chunk->commands()[i];
     //TODO also check register halves
-    if ((cmd.getArgsCount() > 0) && (cmd.getArg(0)->arg_type == ArgType::ARG_REGISTER16)) {
-      auto tst_ref = std::static_pointer_cast<ArgRegister16>(cmd.getArg(0));
-      if (reg == tst_ref->reg_id) {
-        // found register load, need to update right arg (if it's applicable)
-        if (cmd.command_code == CmdCode::LD)
-          if (cmd.getArg(1)->arg_type == ArgType::ARG_DEFAULT) {
-            auto src_ref = std::static_pointer_cast<ArgDefault> (cmd.getArg(1))->getValue();
-            auto lbl = makeData(cmd.addr, src_ref, size == ArgSize::Word ? memory::Reference::Type::READ_WORD : memory::Reference::Type::READ_BYTE);
-            auto src = std::make_shared<ArgMemoryReference>(src_ref, false);
-            src->setLabel(lbl);
-            cmd.setArg(1, src);
+    if (cmd.getArgsCount() > 0) {
+      if (cmd.getArg(0)->arg_type == ArgType::ARG_REGISTER16) {
+        auto tst_ref = std::static_pointer_cast<ArgRegister16>(cmd.getArg(0));
+        if (reg == tst_ref->reg_id) {
+          // found register load, need to update right arg (if it's applicable)
+          if (cmd.command_code == CmdCode::LD) {
+            if (cmd.getArg(1)->arg_type == ArgType::ARG_DEFAULT) {
+              auto src_ref = std::static_pointer_cast<ArgDefault> (cmd.getArg(1))->getValue();
+              auto lbl = makeData(cmd.addr, src_ref, size == ArgSize::Word ? memory::Reference::Type::READ_WORD : memory::Reference::Type::READ_BYTE);
+              auto src = std::make_shared<ArgMemoryReference>(src_ref, false);
+              src->setLabel(lbl);
+              cmd.setArg(1, src);
+            }
           }
-        return;
+          if (cmd.command_code != CmdCode::EX || reg != Register16::DE) {
+            return;
+          }
+        }
+      } else if (cmd.getArg(0)->arg_type == ArgType::ARG_REGISTER8) {
+        auto tst_ref = std::static_pointer_cast<ArgRegister8>(cmd.getArg(0));
+        if (reg.isPart(tst_ref->reg_id)) {
+          // loading part of register, just break search
+          return;
+        }
       }
     }
   }
@@ -424,9 +439,6 @@ size_t DisassemblerCore::postProcessChunk(ChunkPtr chunk, size_t len) {
       len = p->process(chunk, len);
     }
   }
-  if (chunk->addr() == 0x0008) {
-    std::cout << "addr";
-  }
   int idx = 0;
   for (auto& cmd : chunk->commands()) {
     switch (cmd.command_code) {
@@ -434,36 +446,57 @@ size_t DisassemblerCore::postProcessChunk(ChunkPtr chunk, size_t len) {
         updateRegisterSource(chunk, idx, cmd.getArg(0), cmd.getArg(1)->getSize());
         updateRegisterSource(chunk, idx, cmd.getArg(1), cmd.getArg(0)->getSize());
         break;
-      case CmdCode::BIT:
+      case CmdCode::AND:
+      case CmdCode::CP:
+      case CmdCode::DEC:
+      case CmdCode::INC:
+      case CmdCode::OR:
+      case CmdCode::JP:
+      case CmdCode::RL:
+      case CmdCode::RLC:
+      case CmdCode::RR:
+      case CmdCode::RRC:
+      case CmdCode::SLA:
+      case CmdCode::SRA:
+      case CmdCode::SRL:
+      case CmdCode::SUB:
+      case CmdCode::XOR:
         updateRegisterSource(chunk, idx, cmd.getArg(0), ArgSize::Byte);
+        break;
+      case CmdCode::ADC:
+      case CmdCode::ADD:
+      case CmdCode::BIT:
+      case CmdCode::RES:
+      case CmdCode::SBC:
+      case CmdCode::SET:
         updateRegisterSource(chunk, idx, cmd.getArg(1), ArgSize::Byte);
+        break;
+      case CmdCode::CPD:
+      case CmdCode::CPDR:
+      case CmdCode::CPI:
+      case CmdCode::CPIR:
+      case CmdCode::IND:
+      case CmdCode::INDR:
+      case CmdCode::INI:
+      case CmdCode::INIR:
+      case CmdCode::OUTD:
+      case CmdCode::OTDR:
+      case CmdCode::OUTI:
+      case CmdCode::OTIR:
+      case CmdCode::RLD:
+      case CmdCode::RRD:
+        updateRegisterSource(chunk, idx, Register16::HL, ArgSize::Byte);
+        break;
+      case CmdCode::LDD:
+      case CmdCode::LDDR:
+      case CmdCode::LDI:
+      case CmdCode::LDIR:
+        updateRegisterSource(chunk, idx, Register16::HL, ArgSize::Byte);
+        updateRegisterSource(chunk, idx, Register16::DE, ArgSize::Byte);
         break;
     }
     idx++;
   }
-  /*if (cmd.isLDICmd()) {
-    //findAndMarkDEandHL();
-    return len;
-  } else if (cmd.command == "LD") {
-    //check if it's accessed to memory
-    std::string& arg1str = cmd.arg1->toString();
-    if (arg1str.find("(") != arg1str.npos) {
-      if (Register16::getRegister(arg1str) != Register16::None) {
-        //need to find register fill
-      } else {
-        //it's memory address need to create memory label and replace arg with it
-      }
-    } else {
-      std::string& arg2str = cmd.arg2->toString();
-      if (arg2str.find("(") != arg2str.npos) {
-        if (Register16::getRegister(arg2str) != Register16::None) {
-          //need to find register fill
-        } else {
-          //it's memory address need to create memory label and replace arg with it
-        }
-      }
-    }
-  }*/
   return len;
 }
 
--- core/registers.h
+++ core/registers.h
@@ -3,76 +3,6 @@
 namespace dasm {
 namespace core {
 
-class Register16 {
-public:
-  enum Value : uint8_t {
-    None,
-    AF,
-    AF2,
-    BC,
-    DE,
-    HL,
-    SP,
-    IX,
-    IY
-  };
-
-  constexpr Register16(Value reg) : value(reg) {}
-  operator Value() const { return value; }
-
-  static Register16 getRegister(const std::string& arg) {
-    if (arg=="AF") {
-      return Register16::AF;
-    } else if (arg == "AF'") {
-      return Register16::AF2;
-    } else if (arg == "BC") {
-      return Register16::BC;
-    } else if (arg=="DE") {
-      return Register16::DE;
-    } else if (arg=="HL") {
-      return Register16::HL;
-    } else if (arg=="SP") {
-      return Register16::SP;
-    } else if (arg=="IX") {
-      return Register16::IX;
-    } else if (arg=="IY") {
-      return Register16::IY;
-    } else {
-      return Register16::None;
-    }
-  }
-
-
-  std::string toString() const {
-    switch (value) {
-      case None:
-        return "";
-      case AF:
-        return "AF";
-      case AF2:
-        return "AF'";
-      case BC:
-        return "BC";
-      case DE:
-        return "DE";
-      case HL:
-        return "HL";
-      case SP:
-        return "SP";
-      case IX:
-        return "IX";
-      case IY:
-        return "IY";
-      default:
-        return "";
-    }
-  }
-private:
-  Value value;
-};
-
-
-
 class Register8 {
 public:
   enum Value : uint8_t {
@@ -97,7 +27,7 @@ public:
   operator Value() const { return value; }
 
   static Register8 getRegister(const std::string& arg) {
-    if (arg=="A") {
+    if (arg == "A") {
       return Register8::A;
     } else if (arg == "F") {
       return Register8::F;
@@ -170,6 +100,131 @@ public:
 private:
   Value value;
 };
+
+
+class Register16 {
+public:
+  enum Value : uint8_t {
+    None,
+    AF,
+    AF2,
+    BC,
+    DE,
+    HL,
+    SP,
+    IX,
+    IY
+  };
+
+  constexpr Register16(Value reg) : value(reg) {}
+  operator Value() const { return value; }
+
+  Register8 getHight() {
+    switch (value) {
+      case HL:
+        return Register8::H;
+      case AF:
+        return Register8::A;
+      case BC:
+        return Register8::B;
+      case DE:
+        return Register8::D;
+      case IX:
+        return Register8::IXh;
+      case IY:
+        return Register8::IYh;
+    }
+    return Register8::None;
+  }
+
+  Register8 getLow() {
+    switch (value) {
+      case HL:
+        return Register8::L;
+      case AF:
+        return Register8::F;
+      case BC:
+        return Register8::C;
+      case DE:
+        return Register8::E;
+      case IX:
+        return Register8::IXl;
+      case IY:
+        return Register8::IYl;
+    }
+    return Register8::None;
+  }
+
+  bool isPart(Register8 reg) {
+    switch (value) {
+      case HL:
+        return (reg == Register8::L) || (reg == Register8::H);
+      case AF:
+        return (reg == Register8::F) || (reg == Register8::A);
+      case BC:
+        return (reg == Register8::C) || (reg == Register8::B);
+      case DE:
+        return (reg == Register8::E) || (reg == Register8::D);
+      case IX:
+        return (reg == Register8::IXl) || (reg == Register8::IXh);
+      case IY:
+        return (reg == Register8::IYl) || (reg == Register8::IYh);
+    }
+    return false;
+  }
+
+  static Register16 getRegister(const std::string& arg) {
+    if (arg=="AF") {
+      return Register16::AF;
+    } else if (arg == "AF'") {
+      return Register16::AF2;
+    } else if (arg == "BC") {
+      return Register16::BC;
+    } else if (arg=="DE") {
+      return Register16::DE;
+    } else if (arg=="HL") {
+      return Register16::HL;
+    } else if (arg=="SP") {
+      return Register16::SP;
+    } else if (arg=="IX") {
+      return Register16::IX;
+    } else if (arg=="IY") {
+      return Register16::IY;
+    } else {
+      return Register16::None;
+    }
+  }
+
+
+  std::string toString() const {
+    switch (value) {
+      case None:
+        return "";
+      case AF:
+        return "AF";
+      case AF2:
+        return "AF'";
+      case BC:
+        return "BC";
+      case DE:
+        return "DE";
+      case HL:
+        return "HL";
+      case SP:
+        return "SP";
+      case IX:
+        return "IX";
+      case IY:
+        return "IY";
+      default:
+        return "";
+    }
+  }
+private:
+  Value value;
+};
+
+
 
 
 }
